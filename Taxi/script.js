d508c2a03bced1e30daa140b80cf284cff6ad58b
SUGI J <138013066+sugijayachandran@users.noreply.github.com>
2024-07-27 21:44:32 +0530

    Add files via upload

---
+++ Taxi/script.js
@@ -0,0 +1,20 @@
+document.getElementById('quoteForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = {};
+    formData.forEach((value, key) => {
+        if (data[key]) {
+            if (!Array.isArray(data[key])) {
+                data[key] = [data[key]];
+            }
+            data[key].push(value);
+        } else {
+            data[key] = value;
+        }
+    });
+
+    console.log('Form Data Submitted:', data);
+    alert('Quote request submitted!.');
+
+});
